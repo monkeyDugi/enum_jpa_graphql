8f233c784dfe79be592fa9ee68d25ad0dc8c008d
monkeyDugi <lbd4946@gmail.com>
2020-08-01 15:23:36 +0900

    first commit

---
+++ src/main/java/com/example/enum_jpa_graphql/EnumJpaGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.enum_jpa_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumJpaGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnumJpaGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/enum_jpa_graphql/EnumJpaGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enum_jpa_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnumJpaGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
